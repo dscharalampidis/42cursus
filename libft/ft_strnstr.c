efe6ca43c748b3b6326d0cae519a8395fd965386
Dimitrios Charalampidis <d.s.charalampidis@gmail.com>
2022-10-13 08:10:21 +0200

    Fix argument names

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,27 +6,27 @@
 /*   By: dcharala <dcharala@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 06:15:22 by dcharala          #+#    #+#             */
-/*   Updated: 2022/10/13 06:27:40 by dcharala         ###   ########.fr       */
+/*   Updated: 2022/10/13 08:10:03 by dcharala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 char
-	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t	i;
 	size_t	j;
 
-	if (!*needle)
-		return ((char *)haystack);
+	if (!*little)
+		return ((char *)big);
 	i = -1;
-	while (++i < len && haystack[i])
+	while (++i < len && big[i])
 	{
 		j = -1;
-		while (i + ++j < len && needle[j] && haystack[i + j] == needle[j])
-			if (needle[j + 1] == 0)
-				return ((char *)&haystack[i]);
+		while (i + ++j < len && little[j] && big[i + j] == little[j])
+			if (little[j + 1] == 0)
+				return ((char *)&big[i]);
 	}
 	return (NULL);
 }
