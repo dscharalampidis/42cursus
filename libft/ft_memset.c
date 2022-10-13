572096b989c8ec9e6962335c814ff9332630fa7f
Dimitrios Charalampidis <d.s.charalampidis@gmail.com>
2022-10-13 03:25:35 +0200

    Add ft_memset
    I also edited the libft.h header file to comply with norminette.

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -1,25 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dcharala <dcharala@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/13 01:33:45 by dcharala          #+#    #+#             */
-/*   Updated: 2022/10/13 03:24:24 by dcharala         ###   ########.fr       */
+/*   Created: 2022/10/13 03:19:48 by dcharala          #+#    #+#             */
+/*   Updated: 2022/10/13 03:21:43 by dcharala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <stddef.h>
+#include "libft.h"
 
-int		ft_isalpha(int chr);
-int		ft_isdigit(int chr);
-int		ft_isalnum(int chr);
-int		ft_isascii(int chr);
-int		ft_isprint(int chr);
-size_t	ft_strlen(const char *str);
-void	*ft_memset(void *buf, int chr, size_t len);
+void
+	*ft_memset(void *buf, int chr, size_t len)
+{
+	size_t	i;
+	char	*str;
 
-#endif
+	i = 0;
+	str = (char *)buf;
+	while (i < len)
+	{
+		str[i] = chr;
+		i++;
+	}
+	return (str);
+}
