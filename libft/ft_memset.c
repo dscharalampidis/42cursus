b4410903e5dcf485b39a3d55ba49ce3327589e34
Dimitrios Charalampidis <d.s.charalampidis@gmail.com>
2022-10-13 08:01:00 +0200

    Fix argument names

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -6,21 +6,21 @@
 /*   By: dcharala <dcharala@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 03:19:48 by dcharala          #+#    #+#             */
-/*   Updated: 2022/10/13 06:11:09 by dcharala         ###   ########.fr       */
+/*   Updated: 2022/10/13 08:00:54 by dcharala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 void
-	*ft_memset(void *ptr, int chr, size_t n)
+	*ft_memset(void *s, int c, size_t n)
 {
 	size_t	i;
 	char	*str;
 
-	str = (char *)ptr;
+	str = (char *)s;
 	i = -1;
 	while (++i < n)
-		str[i] = chr;
+		str[i] = c;
 	return (str);
 }
