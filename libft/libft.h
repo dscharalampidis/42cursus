40c9fea942a8ec8c4c86f9f2475db04a65283872
Dimitrios Charalampidis <d.s.charalampidis@gmail.com>
2022-10-13 04:51:38 +0200

    Add ft_strlcat

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: dcharala <dcharala@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 01:33:45 by dcharala          #+#    #+#             */
-/*   Updated: 2022/10/13 04:36:30 by dcharala         ###   ########.fr       */
+/*   Updated: 2022/10/13 04:45:30 by dcharala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,6 @@ void	ft_bzero(void *ptr, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t siz);
+size_t	ft_strlcat(char *dst, const char *src, size_t siz);
 
 #endif
