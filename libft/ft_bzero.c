d1a28fa0dc433f3fd51b0498ebb242bb931decc4
Dimitrios Charalampidis <d.s.charalampidis@gmail.com>
2022-10-13 03:49:01 +0200

    Add ft_bzero

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -1,26 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dcharala <dcharala@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/13 01:33:45 by dcharala          #+#    #+#             */
-/*   Updated: 2022/10/13 03:37:52 by dcharala         ###   ########.fr       */
+/*   Created: 2022/10/13 03:32:25 by dcharala          #+#    #+#             */
+/*   Updated: 2022/10/13 03:39:25 by dcharala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <stddef.h>
+#include "libft.h"
 
-int		ft_isalpha(int chr);
-int		ft_isdigit(int chr);
-int		ft_isalnum(int chr);
-int		ft_isascii(int chr);
-int		ft_isprint(int chr);
-size_t	ft_strlen(const char *str);
-void	*ft_memset(void *buf, int chr, size_t len);
-void	ft_bzero(void *ptr, size_t n);
+void
+	ft_bzero(void *ptr, size_t n)
+{
+	size_t	i;
+	char	*str;
 
-#endif
+	i = 0;
+	str = (char *)ptr;
+	while (i < n)
+		str[i++] = 0;
+}
