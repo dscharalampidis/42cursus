b5d001613980d46626397e5282c5c822c0fd8969
Dimitrios Charalampidis <d.s.charalampidis@gmail.com>
2022-10-13 08:04:58 +0200

    Fix argument names

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,14 +6,14 @@
 /*   By: dcharala <dcharala@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 04:39:52 by dcharala          #+#    #+#             */
-/*   Updated: 2022/10/13 04:50:40 by dcharala         ###   ########.fr       */
+/*   Updated: 2022/10/13 08:04:51 by dcharala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 size_t
-	ft_strlcat(char *dst, const char *src, size_t siz)
+	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 	size_t	dst_len;
@@ -21,10 +21,10 @@ size_t
 
 	dst_len = ft_strlen(dst);
 	src_len = ft_strlen((char *)src);
-	if (siz < dst_len)
-		return (src_len + siz);
+	if (size < dst_len)
+		return (src_len + size);
 	i = -1;
-	while (dst_len + 1 + ++i < siz && src[i] != 0)
+	while (dst_len + 1 + ++i < size && src[i] != 0)
 		dst[dst_len + i] = src[i];
 	dst[dst_len + i] = 0;
 	return (dst_len + src_len);
