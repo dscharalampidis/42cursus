7da302481c19e2756871a4434e407fb0eedf96a2
Dimitrios Charalampidis <d.s.charalampidis@gmail.com>
2022-10-13 08:08:38 +0200

    Fix argument names

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,21 +6,21 @@
 /*   By: dcharala <dcharala@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 05:40:46 by dcharala          #+#    #+#             */
-/*   Updated: 2022/10/13 05:58:07 by dcharala         ###   ########.fr       */
+/*   Updated: 2022/10/13 08:08:33 by dcharala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 int
-	ft_strncmp(const char *str1, const char *str2, size_t n)
+	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
 	if (n == 0)
 		return (0);
 	i = -1;
-	while (++i < n - 1 && str1[i] != 0 && str2[i] != 0 && str1[i] == str2[i])
+	while (++i < n - 1 && s1[i] != 0 && s2[i] != 0 && s1[i] == s2[i])
 		;
-	return ((unsigned char)str1[i] - (unsigned char)str2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
