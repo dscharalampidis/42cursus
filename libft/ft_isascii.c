addff200f09ded4b6294316c0d09a6be0774a6f2
Dimitrios Charalampidis <d.s.charalampidis@gmail.com>
2022-10-13 03:00:53 +0200

    Add ft_isascii

--- libft/ft_isascii.c
+++ libft/ft_isascii.c
@@ -1,25 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dcharala <dcharala@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/13 01:33:45 by dcharala          #+#    #+#             */
-/*   Updated: 2022/10/13 02:59:34 by dcharala         ###   ########.fr       */
+/*   Created: 2022/10/13 02:58:00 by dcharala          #+#    #+#             */
+/*   Updated: 2022/10/13 02:59:16 by dcharala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
 int
-	ft_isalpha(int chr);
-int
-	ft_isdigit(int chr);
-int
-	ft_isalnum(int chr);
-int
-	ft_isascii(int chr);
-
-#endif
+	ft_isascii(int chr)
+{
+	if (chr >= 0 && chr <= 127)
+		return (1);
+	else
+		return (0);
+}
