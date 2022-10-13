9b8a0a76ab9c8ebb9b9607323d95947520a39ce7
Dimitrios Charalampidis <d.s.charalampidis@gmail.com>
2022-10-13 07:58:57 +0200

    Fix argument names

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,25 +6,25 @@
 /*   By: dcharala <dcharala@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 03:56:42 by dcharala          #+#    #+#             */
-/*   Updated: 2022/10/13 04:32:37 by dcharala         ###   ########.fr       */
+/*   Updated: 2022/10/13 07:58:51 by dcharala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 void
-	*ft_memcpy(void *dst, const void *src, size_t n)
+	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	i;
 	char	*d;
 	char	*s;
 
-	d = (char *)dst;
+	d = (char *)dest;
 	s = (char *)src;
 	if (s == NULL && d == NULL)
 		return (0);
 	i = -1;
 	while (++i < n)
 		d[i] = s[i];
-	return (dst);
+	return (dest);
 }
