8cd694512fc480b86492c6b11a93ef0bd5aeca3a
Dimitrios Charalampidis <d.s.charalampidis@gmail.com>
2022-10-13 05:58:55 +0200

    Fix norminette error

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: dcharala <dcharala@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 05:40:46 by dcharala          #+#    #+#             */
-/*   Updated: 2022/10/13 05:49:12 by dcharala         ###   ########.fr       */
+/*   Updated: 2022/10/13 05:58:07 by dcharala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ int
 	if (n == 0)
 		return (0);
 	i = -1;
-	while (++i < n - 1 && str1[i] != 0 && str2[i] != 0 && str1[i] == str2[i]);
+	while (++i < n - 1 && str1[i] != 0 && str2[i] != 0 && str1[i] == str2[i])
+		;
 	return ((unsigned char)str1[i] - (unsigned char)str2[i]);
 }
