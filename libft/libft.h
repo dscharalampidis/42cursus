ac900b2b3691ed4fe212c697e2acf9acc0ee0d5c
Dimitrios Charalampidis <d.s.charalampidis@gmail.com>
2022-10-13 05:27:19 +0200

    Add ft_tolower

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: dcharala <dcharala@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 01:33:45 by dcharala          #+#    #+#             */
-/*   Updated: 2022/10/13 05:22:23 by dcharala         ###   ########.fr       */
+/*   Updated: 2022/10/13 05:26:24 by dcharala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,6 @@ void	*ft_memmove(void *dst, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t siz);
 size_t	ft_strlcat(char *dst, const char *src, size_t siz);
 int		ft_toupper(int chr);
+int		ft_tolower(int chr);
 
 #endif
