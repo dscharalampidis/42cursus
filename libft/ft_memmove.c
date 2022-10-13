6b5e69b955c460354fe5367a710a72d9c69ece50
Dimitrios Charalampidis <d.s.charalampidis@gmail.com>
2022-10-13 04:35:27 +0200

    Fix norminette errors

---
+++ libft/ft_memmove.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcharala <dcharala@student.42heilbronn.de  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/13 04:13:01 by dcharala          #+#    #+#             */
+/*   Updated: 2022/10/13 04:34:43 by dcharala         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void
+	*ft_memmove(void *dst, const void *src, size_t n)
+{
+	char	*d;
+	char	*s;
+
+	d = (char *)dst;
+	s = (char *)src;
+	if (dst == NULL && src == NULL)
+		return (NULL);
+	if (d > s)
+	{
+		while (n > 0)
+		{
+			d[n - 1] = s[n - 1];
+			n--;
+		}
+	}
+	else
+		d = ft_memcpy(dst, src, n);
+	return (dst);
+}
