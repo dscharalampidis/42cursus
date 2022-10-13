441d1be1d24825b21030ad5d8702a8e3d5ca445a
Dimitrios Charalampidis <d.s.charalampidis@gmail.com>
2022-10-13 07:56:31 +0200

    Fix argument names

--- libft/ft_isascii.c
+++ libft/ft_isascii.c
@@ -6,16 +6,16 @@
 /*   By: dcharala <dcharala@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 02:58:00 by dcharala          #+#    #+#             */
-/*   Updated: 2022/10/13 02:59:16 by dcharala         ###   ########.fr       */
+/*   Updated: 2022/10/13 07:56:19 by dcharala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 int
-	ft_isascii(int chr)
+	ft_isascii(int c)
 {
-	if (chr >= 0 && chr <= 127)
+	if (c >= 0 && c <= 127)
 		return (1);
 	else
 		return (0);
