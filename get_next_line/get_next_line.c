4050c698a48d97182fb792f880412986e6cd5726
Dimitrios Charalampidis <d.s.charalampidis@gmail.com>
2022-10-14 19:46:05 +0200

    Fix coding style

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,41 +6,37 @@
 /*   By: dcharala <dcharala@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/28 03:53:06 by dcharala          #+#    #+#             */
-/*   Updated: 2022/08/29 06:10:55 by dcharala         ###   ########.fr       */
+/*   Updated: 2022/10/14 19:42:03 by dcharala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-/* Find a character in a string. This function is used to find a newline
- * character in the buffer. */
 char
-	*ft_strchr(const char *str, int c)
+	*ft_strchr(const char *s, int c)
 {
-	while (*str != (char)c && *str)
-		str++;
-	if (*str == (char)c)
-		return ((char *)str);
+	while (*s != (unsigned char)c && *s)
+		s++;
+	if (*s == (unsigned char)c)
+		return ((char *)s);
 	return (NULL);
 }
 
-/* Append each `buf` from `read()` to `joinedbuf` until there's at least one
- * newline character in it, or the file ends. */
 char
 	*append_buf(int fd, char *buf, char *joinedbuf)
 {
 	char	*tmp;
-	int		bufsiz;
+	int		size;
 
-	bufsiz = 1;
-	while (bufsiz)
+	size = 1;
+	while (size)
 	{
-		bufsiz = read(fd, buf, BUFFER_SIZE);
-		if (bufsiz == -1)
+		size = read(fd, buf, BUFFER_SIZE);
+		if (size == -1)
 			return (NULL);
-		else if (bufsiz == 0)
+		else if (size == 0)
 			break ;
-		buf[bufsiz] = '\0';
+		buf[size] = '\0';
 		if (!joinedbuf)
 			joinedbuf = ft_strdup("");
 		tmp = joinedbuf;
@@ -53,18 +49,15 @@ char
 	return (joinedbuf);
 }
 
-/* Separate the `line` to be printed from the `joinedbuf` by adding a
- * terminating null after the newline character. Then return the rest of the
- * `joinedbuf`. */
 char
 	*separate_line_from_joinedbuf(char *line)
 {
 	size_t	i;
 	char	*joinedbuf;
 
-	i = 0;
-	while (line[i] != '\n' && line[i])
-		i++;
+	i = -1;
+	while (line[++i] != '\n' && line[i])
+		;
 	if (!line[i])
 		return (NULL);
 	joinedbuf = ft_substr(line, i + 1, ft_strlen(line) - i);
@@ -77,8 +70,6 @@ char
 	return (joinedbuf);
 }
 
-/* Get and print a line from the `joinedbuf` until there's nothing left in it.
- * */
 char
 	*get_next_line(int fd)
 {
