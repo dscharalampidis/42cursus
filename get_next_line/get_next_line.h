3ad15d0c9bac7654f934ced1020c229c8a32c535
Dimitrios Charalampidis <d.s.charalampidis@gmail.com>
2022-10-14 20:04:06 +0200

    Fix argument names

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: dcharala <dcharala@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/28 03:32:35 by dcharala          #+#    #+#             */
-/*   Updated: 2022/09/06 05:33:08 by dcharala         ###   ########.fr       */
+/*   Updated: 2022/10/14 20:03:38 by dcharala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,29 +31,21 @@
 #  include <limits.h>
 # endif
 
-/* BUFSIZ is the default buffer size of the OS defined in the stdio.h */
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE BUFSIZ
 # endif
 
-/* If BUFFER_SIZE is defined by the user to a value that results in exceeding
- * the maximum size of any variable in the program the compiler will complain
- * and set it to an unpredicable value. The below code set the buffer size to
- * zero when the user enters a number higher than the maximum integer to avoid
- * unexpected behaviour when malloc-ing a sizeof(char). The -1 makes sure no
- * overflow occurs when we add +1 for the terminating null. Check
- * get_next_line() for details.*/
 # if BUFFER_SIZE > INT_MAX - 1
 #  undef BUFFER_SIZE
 #  define BUFFER_SIZE 0
 # endif
 
-char	*ft_strchr(const char *str, int c);
-size_t	ft_strlen(const char *str);
-char	*ft_strdup(const char *str);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-char	*ft_strjoin(const char *str1, const char *str2);
-char	*ft_substr(char const *str, unsigned int start, size_t n);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t n);
 char	*append_buf(int fd, char *buf, char *joinedbuf);
 char	*separate_line_from_joinedbuf(char *line);
 char	*get_next_line(int fd);
