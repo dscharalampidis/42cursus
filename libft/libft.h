d6ba0a994c22e028e863b47a6082e3cbcb15e73b
Dimitrios Charalampidis <d.s.charalampidis@gmail.com>
2022-10-14 13:11:23 +0200

    Add ft_putendl_fd

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: dcharala <dcharala@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 01:33:45 by dcharala          #+#    #+#             */
-/*   Updated: 2022/10/14 13:00:33 by dcharala         ###   ########.fr       */
+/*   Updated: 2022/10/14 13:04:31 by dcharala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,5 +45,6 @@ char	*ft_itoa(int n);
 char	*ft_strmapi(const char *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 void	ft_putchar_fd(char c, int fd);
+void	ft_putendl_fd(char *s, int fd);
 
 #endif
