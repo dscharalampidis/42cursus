b3fd6d34e076ff40e62cd638857e72c812c34ee3
Dimitrios Charalampidis <d.s.charalampidis@gmail.com>
2022-10-17 05:36:23 +0200

    Update selection sort algorithm

--- push_swap/sorting_algorithms/selection_sort.c
+++ push_swap/sorting_algorithms/selection_sort.c
@@ -1,33 +1,13 @@
 #include <stdio.h>
 
 void
-	swap(int *xp, int *yp)
+	swap(int *x, int *y)
 {
 	int	tmp;
 
-	tmp = *xp;
-	*xp = *yp;
-	*yp = tmp;
-}
-
-void
-	selection_sort(int *arr, int n)
-{
-	int	i;
-	int	j;
-	int	min;
-
-	i = -1;
-	while (++i < n - 1)
-	{
-		min = i;
-		j = i;
-		while (++j < n)
-			if (arr[j] < arr[min])
-				min = j;
-		if (min != i)
-			swap(&arr[min], &arr[i]);
-	}
+	tmp = *x;
+	*x = *y;
+	*y = tmp;
 }
 
 void
@@ -41,20 +21,74 @@ void
 	printf("\n");
 }
 
+void
+	selection_sort(int *arr, int n)
+{
+	int	i;
+	int	j;
+	int	min;
+
+	i = -1;
+	while (++i < n - 1)
+	{
+		printf("Comparing %d against the elements after it...\n", arr[i]);
+		min = i;
+		j = i;
+		while (++j < n)
+		{
+			printf("Is %d smaller than %d? ", arr[min], arr[j]);
+			if (arr[min] > arr[j])
+			{
+				min = j;
+				printf("No, the smallest element is now %d.\n", arr[min]);
+			}
+			else
+				printf("Yes, %d is still the smallest.\n", arr[min]);
+		}
+		if (min != i)
+		{
+			printf("Swap %d with %d.\n", arr[min], arr[i]);
+			swap(&arr[min], &arr[i]);
+			printf("\nThe array now looks like this:\n");
+			print_arr(arr, n);
+			printf("\n");
+		}
+	}
+}
+
 int
 	main(void)
 {
-	int	arr[5];
+	int	e;
 	int	n;
+	int	arr[100];
+	int	i;
 
-	arr[0] = 64;
-	arr[1] = 25;
-	arr[2] = 12;
-	arr[3] = 22;
-	arr[4] = 11;
-	n = sizeof(arr) / sizeof(arr[0]);
-	selection_sort(arr, n);
+	printf("How many elements would you like to enter?\n");
+	if (scanf("%d", &e) != 1)
+	{
+		fprintf(stderr, "Bad data entry. Exiting...\n");
+		return (1);
+	}
+	n = (int)e;
+	i = 1;
+	while (i <= n)
+	{
+		printf("Enter an integer for the element %d of %d:\n", i, n);
+		if (scanf("%d", &arr[i - 1]) != 1)
+		{
+			fprintf(stderr, "Bad data entry. Exiting...\n");
+			return (1);
+		}
+		i++;
+	}
+	printf("The original array is:\n");
+	print_arr(arr, n);
 	printf("\n");
+	printf("Starting selection sort...\n");
+	printf("Assume the first element of the array is the smallest.\n");
+	selection_sort(arr, n);
+	printf("The sorted array looks like this:\n");
 	print_arr(arr, n);
 	return (0);
 }
