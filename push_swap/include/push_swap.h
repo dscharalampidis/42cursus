988b7c9cb00a6be0406e4d2df8fba64549dd2ac6
Dimitrios Charalampidis <d.s.charalampidis@gmail.com>
2022-10-18 18:45:48 +0200

    Add fn to copy argv to arr
    I created a function that copies the argv into char *arr adding a
    delimiter between the arguments.

--- push_swap/include/push_swap.h
+++ push_swap/include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: dcharala <dcharala@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 17:59:33 by dcharala          #+#    #+#             */
-/*   Updated: 2022/10/17 18:00:25 by dcharala         ###   ########.fr       */
+/*   Updated: 2022/10/18 18:45:09 by dcharala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@
 # include "../libft/include/libft.h"
 
 /* push_swap.c */
+char	*ps_conv_argvtoarr(char **argv, char *delim);
 
 #endif
--- push_swap/src/push_swap.c
+++ push_swap/src/push_swap.c
@@ -6,14 +6,41 @@
 /*   By: dcharala <dcharala@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 18:10:03 by dcharala          #+#    #+#             */
-/*   Updated: 2022/10/17 18:34:15 by dcharala         ###   ########.fr       */
+/*   Updated: 2022/10/18 18:43:33 by dcharala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int
-	main(void)
+char *
+	ps_conv_argvtoarr(char **argv, char *delim)
 {
+	int		i;
+	char	*tmp;
+	char	*arr;
+
+	i = 1;
+	arr = ft_strdup(argv[i]);
+	while (argv[++i])
+	{
+		tmp = arr;
+		arr = ft_strjoin(arr, delim);
+		free(tmp);
+		tmp = arr;
+		arr = ft_strjoin(arr, argv[i]);
+		free(tmp);
+	}
+	return (arr);
+}
+
+int
+	main(int argc, char **argv)
+{
+	char	*arr;
+
+	if (argc > 1)
+		arr = ps_conv_argvtoarr(argv, "-");
+	ft_printf("%s\n", arr);
+	free(arr);
 	return (0);
 }
