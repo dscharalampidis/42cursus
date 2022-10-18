b6952708e8b9d086d7a58e4f980b80e30256fb52
Dimitrios Charalampidis <d.s.charalampidis@gmail.com>
2022-10-18 21:09:09 +0200

    Add fn to check for invalid args

--- push_swap/src/push_swap.c
+++ push_swap/src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: dcharala <dcharala@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 18:10:03 by dcharala          #+#    #+#             */
-/*   Updated: 2022/10/18 19:56:16 by dcharala         ###   ########.fr       */
+/*   Updated: 2022/10/18 21:04:17 by dcharala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,51 @@ char**
 	return (arrv);
 }
 
+void
+	ps_error(void)
+{
+	ft_putstr_fd("Error\n", 2);
+	exit(EXIT_FAILURE);
+}
+
+void
+	ps_free(char **arrv, char error)
+{
+	int	i;
+
+	i = -1;
+	while (arrv[++i])
+		free(arrv[i]);
+	free(arrv);
+	if (error == 'E')
+		ps_error();
+}
+
+void
+	ps_chk_invalid_arg(char **arrv)
+{
+	int			i;
+	int			j;
+	long long	nbr;
+
+	i = -1;
+	while (arrv[++i])
+	{
+		j = -1;
+		while (arrv[i][++j])
+		{
+			if (arrv[i][j] == 43 || arrv[i][j] == 45)
+				j++;
+			if (!(ft_isdigit(arrv[i][j])))
+				ps_free(arrv, 'E');
+			j++;
+		}
+		nbr = ft_atoi(arrv[i]);
+		if (nbr > INT_MAX || nbr < INT_MIN)
+			ps_free(arrv, 1);
+	}
+}
+
 int
 	main(int argc, char **argv)
 {
@@ -45,6 +90,7 @@ int
 	arrv = NULL;
 	if (argc > 1)
 		arrv = ps_conv_argvtoarrv(argv);
+	ps_chk_invalid_arg(arrv);
 	i = -1;
 	while (arrv[++i])
 	{
