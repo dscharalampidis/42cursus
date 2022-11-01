ed12fbd71bdf1e266ad5e6622bd80d6dd7644016
Dimitrios Charalampidis <d.s.charalampidis@gmail.com>
2022-11-01 16:12:18 +0100

    Fix sorting algo and add an exclusion for two numbers

--- push_swap/src/push_swap.c
+++ push_swap/src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: dcharala <dcharala@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 18:10:03 by dcharala          #+#    #+#             */
-/*   Updated: 2022/10/31 22:25:11 by dcharala         ###   ########.fr       */
+/*   Updated: 2022/11/01 15:58:00 by dcharala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,27 +62,54 @@ int
 	return (min_i);
 }
 
+int
+	ps_chk_lst_sorted(t_node *head)
+{
+	if (head == NULL)
+		return 0;
+	while (head->nxt)
+	{
+		if (head->nbr > head->nxt->nbr)
+			return 1;
+		head = head->nxt;
+	}
+	return 0;
+}
+
 void
 	ps_trytosort(t_node **stack_a, t_node **stack_b, int min_i, int lstlen)
 {
-	while (min_i != 1 && *stack_a)
+	while (ps_chk_lst_sorted(*stack_a) != 0 && *stack_a)
 	{
 		if ((*stack_a)->nxt->nbr < (*stack_a)->nbr)
 		{
 			printf("sa\n");
 			ps_swap(stack_a);
 			min_i = ps_find_min_pos(*stack_a);
-			printf("new min_i: %d\n", min_i);
+			/* printf("--- Stack A ---\n"); */
+			/* ps_print_lst(*stack_a); */
+			/* printf("--- Stack B ---\n"); */
+			/* ps_print_lst(*stack_b); */
 		}
 		if (min_i != 1 && min_i <= lstlen / 2 + 1)
 		{	
 			printf("ra\n");
 			ps_rotate_lst(stack_a);
+			min_i = ps_find_min_pos(*stack_a);
+			/* printf("--- Stack A ---\n"); */
+			/* ps_print_lst(*stack_a); */
+			/* printf("--- Stack B ---\n"); */
+			/* ps_print_lst(*stack_b); */
 		}
 		else if (min_i != 1 && min_i > lstlen / 2 + 1)
 		{
 			printf("rra\n");
 			ps_reverse_rotate_lst(stack_a);
+			min_i = ps_find_min_pos(*stack_a);
+			/* printf("--- Stack A ---\n"); */
+			/* ps_print_lst(*stack_a); */
+			/* printf("--- Stack B ---\n"); */
+			/* ps_print_lst(*stack_b); */
 		}
 		else
 		{
@@ -90,7 +117,21 @@ void
 			ps_push(stack_a, stack_b);
 			lstlen = ps_find_lst_len(*stack_a);
 			min_i = ps_find_min_pos(*stack_a);
+			/* printf("--- Stack A ---\n"); */
+			/* ps_print_lst(*stack_a); */
+			/* printf("--- Stack B ---\n"); */
+			/* ps_print_lst(*stack_b); */
 		}
+	}
+}
+
+void
+	ps_backtoa(t_node **stack_a, t_node **stack_b)
+{
+	while (*stack_b)
+	{
+		printf("pa\n");
+		ps_push(stack_b, stack_a);
 	}
 }
 
@@ -116,16 +157,28 @@ int
 		ps_insert_int_lst_tail(&stack_a, arr[i]);
 	free(arr);
 	lstlen = ps_find_lst_len(stack_a);
-	printf("--- Stack A ---\n");
-	ps_print_lst(stack_a);
+	/* printf("--- Original Stack A ---\n"); */
+	/* ps_print_lst(stack_a); */
 	min_i = ps_find_min_pos(stack_a);
-	printf("min_i: %d\n", min_i);
+	/* printf("min_i: %d\n", min_i); */
+	if (stack_a->nxt->nbr < stack_a->nbr && lstlen == 2)
+	{
+		printf("sa\n");
+		ps_swap(&stack_a);
+		return (0);
+	}
 	ps_trytosort(&stack_a, &stack_b, min_i, lstlen);
-	printf("--- Stack A ---\n");
-	ps_print_lst(stack_a);
-	printf("--- Stack B ---\n");
-	ps_print_lst(stack_b);
-/* printf("Push from A to B\n"); */
+	/* printf("--- Stack A ---\n"); */
+	/* ps_print_lst(stack_a); */
+	/* printf("--- Stack B ---\n"); */
+	/* ps_print_lst(stack_b); */
+	ps_backtoa(&stack_a, &stack_b);
+	/* printf("--- Stack A ---\n"); */
+	/* ps_print_lst(stack_a); */
+	/* printf("--- Stack B ---\n"); */
+	/* ps_print_lst(stack_b); */
+	/* printf("%d\n", ps_chk_lst_sorted(stack_a)); */
+	/* printf("Push from A to B\n"); */
 	/* ps_push(&stack_a, &stack_b); */
 	/* printf("Printing stack B\n"); */
 	/* ps_print_lst(stack_b); */
