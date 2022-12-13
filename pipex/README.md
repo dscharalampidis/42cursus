00c1486abfa74b8af581095782888432720a2b31
Dimitrios Charalampidis <d.s.charalampidis@gmail.com>
2022-12-13 03:51:05 +0100

    Initial commit

---
+++ pipex/README.md
@@ -0,0 +1,5 @@
+# pipex [version 2]
+
+## Summary
+This project will let you discover in detail a UNIX mechanism that you already
+know by using it in your program.
---
+++ pipex/include/pipex.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcharala <dcharala@student.42heilbronn.de  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/13 03:14:03 by dcharala          #+#    #+#             */
+/*   Updated: 2022/12/13 03:48:09 by dcharala         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "../libft/include/libft.h"
+
+/* errors.c */
+void	chk_args(int argc);
+
+/* processes.c */
+
+/* utilities.c */
+
+#endif
+
